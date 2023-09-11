5515a51e533656e34b8b52ef65674a6b785056e2
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2023-09-12 02:22:50 +0530

    first commit

---
+++ mark_sheet/script.js
@@ -0,0 +1,62 @@
+class StudentMarksheet {
+  constructor() {
+      this.subjects = [
+          { name: 'Math', id: 'math', marks: 0 },
+          { name: 'Science', id: 'science', marks: 0 },
+          { name: 'English', id: 'english', marks: 0 },
+          { name: 'History', id: 'history', marks: 0 },
+          { name: 'Geography', id: 'geography', marks: 0 }
+      ];
+  }
+
+  calculateDivision(percentage) {
+      if (percentage >= 60) {
+          return "1st Division";
+      } else if (percentage >= 45) {
+          return "2nd Division";
+      } else if (percentage >= 30) {
+          return "3rd Division";
+      } else {
+          return "Fail";
+      }
+  }
+
+  getMarks() {
+      for (let subject of this.subjects) {
+          let marks = parseInt(prompt(`Enter marks for ${subject.name}:`));
+
+          if (!isNaN(marks) && marks >= 0 && marks <= 100) {
+              subject.marks = marks;
+          } else {
+              console.log(
+                  "Invalid input for marks. Please enter a number between 0 and 100."
+              );
+              return false;
+          }
+      }
+      return true;
+  }
+
+  displayResult() {
+      let totalMarks = 0;
+
+      for (let subject of this.subjects) {
+          totalMarks += subject.marks;
+
+          let percentage = (subject.marks / 100) * 100;
+          let division = this.calculateDivision(percentage);
+
+          document.getElementById(`${subject.id}Marks`).textContent = subject.marks;
+          document.getElementById(`${subject.id}Percentage`).textContent = `${percentage.toFixed(0)}%`;
+          document.getElementById(`${subject.id}Division`).textContent = division;
+      }
+  }
+}
+
+const student = new StudentMarksheet();
+
+if (student.getMarks()) {
+  student.displayResult();
+} else {
+  console.log("Invalid input. Please try again.");
+}
